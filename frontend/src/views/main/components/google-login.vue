ebc4f4ab70f258d6eef997472dbeaf876f64ebc5
김채연 <86418158+chaeyeonkim0223@users.noreply.github.com>
2021-07-26 17:34:30 +0900

    S05P12B101-65 구글로그인 컴포넌트 생성

---
+++ frontend/src/views/main/components/google-login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div id="google-signin-btn"></div>
+    <button type="button" @click="signOut">로그아웃</button>
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    window.gapi.signin2.render("google-signin-btn", {
+      onsuccess: this.onSignIn,
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: "dark"
+    });
+  },
+  methods: {
+    onSignIn(googleUser) {
+      const profile = googleUser.getBasicProfile();
+      console.log("ID: " + profile.getId());
+      console.log("Full Name: " + profile.getName());
+      console.log("Given Name: " + profile.getGivenName());
+      console.log("Family Name: " + profile.getFamilyName());
+      console.log("Image URL: " + profile.getImageUrl());
+      console.log("Email: " + profile.getEmail());
+
+      const id_token = googleUser.getAuthResponse().id_token;
+      console.log("ID Token: " + id_token);
+    },
+    signOut() {
+      window.gapi.auth2.getAuthInstance().disconnect();
+    }
+  }
+};
+</script>
