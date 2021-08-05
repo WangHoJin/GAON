94d012cbe354a3b59c5978a077629bdda1248e4d
co323co <co323co@gmail.com>
2021-08-06 03:09:48 +0900

    jar 업로드

--- backend/src/main/java/com/ssafy/db/repository/RoomRepositorySupport.java
+++ backend/src/main/java/com/ssafy/db/repository/RoomRepositorySupport.java
@@ -10,10 +10,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 방 모델 관련 디비 쿼리 생성을 위한 구현 정의.
  */
+@Transactional
 @Repository
 public class RoomRepositorySupport {
     @Autowired
