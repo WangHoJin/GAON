5f4e89893bc4f6660d032080bf20c59351ffbfed
Su-bro <tngud941208@naver.com>
2021-08-06 10:19:23 +0900

    [S05P13B101-26] BE : 출석부 엔티티 생성

---
+++ backend/src/main/java/com/ssafy/db/entity/RollBook.java
@@ -0,0 +1,54 @@
+package com.ssafy.db.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ * 방 모델 정의.
+ */
+@Entity
+@Getter
+@Setter
+//Entity 영속성 및 업데이트에 대한 Auditing 정보를 캡처하는 JPA Entity Listener
+@EntityListeners(AuditingEntityListener.class)
+public class RollBook extends BaseEntity{    
+	
+	@ManyToOne(cascade=CascadeType.REMOVE,fetch=FetchType.LAZY)
+    @JoinColumn(name="room_id", nullable=false)
+	@OnDelete(action=OnDeleteAction.CASCADE)
+    Room room;
+	
+    
+	@ManyToOne(cascade=CascadeType.REMOVE,fetch=FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable=false)
+    @OnDelete(action=OnDeleteAction.CASCADE)
+    Guser user;
+	
+	Date date;
+	
+	String state;
+
+}
