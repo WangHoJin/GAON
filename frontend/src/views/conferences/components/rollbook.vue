26215fcde35fa2244d18d9aee791c00baf0dfe8b
Su-bro <tngud941208@naver.com>
2021-08-11 01:12:04 +0900

    [S05P13B101-30] FE : 달력 클릭시 해당 날짜 출석정보 GET

--- frontend/src/views/conferences/components/rollbook.vue
+++ frontend/src/views/conferences/components/rollbook.vue
@@ -3,79 +3,107 @@
     <el-contianer class="main-container">
       <el-main>
         <el-calendar>
-        <template #dateCell="{data}">
-          <div id="calendar-day" :class="data.isSelected ? 'is-selected' : ''" @click="selectday(data.day)">
-            {{ data.day.split('-').slice(1).join('-') }} {{ data.isSelected ? '✔️' : '' }}
-          </div>
-        </template>
-      </el-calendar>
+          <template #dateCell="{data}">
+            <div
+              id="calendar-day"
+              :class="data.isSelected ? 'is-selected' : ''"
+              @click="selectday(data.day)"
+            >
+              {{
+                data.day
+                  .split("-")
+                  .slice(1)
+                  .join("-")
+              }}
+              {{ data.isSelected ? "✔️" : "" }}
+            </div>
+          </template>
+        </el-calendar>
 
         <el-table
-        :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
-        style="width: 100%">
-        <el-table-column
-          label="이름"
-          prop="name">
-        </el-table-column>
-        <el-table-column
-          align="right"
-          >
-          <template #header>
-            <el-input
-              v-model="search"
-              size="mini"
-              placeholder="Type to search"/>
-          </template>
-          <template #default="scope">
-            <el-radio v-model="tableData[scope.$index].attendance" label="1">출석</el-radio>
-            <el-radio v-model="tableData[scope.$index].attendance" label="0">결석</el-radio>
-          </template>
-        </el-table-column>
-      </el-table>
+          :data="
+            tableData.filter(
+              data =>
+                !search ||
+                data.name.toLowerCase().includes(search.toLowerCase())
+            )
+          "
+          style="width: 100%"
+        >
+          <el-table-column label="이름" prop="name"> </el-table-column>
+          <el-table-column align="right">
+            <template #header>
+              <el-input
+                v-model="search"
+                size="mini"
+                placeholder="Type to search"
+              />
+            </template>
+            <template #default="scope">
+              <el-radio v-model="tableData[scope.$index].attendance" label="1"
+                >출석</el-radio
+              >
+              <el-radio v-model="tableData[scope.$index].attendance" label="0"
+                >결석</el-radio
+              >
+            </template>
+          </el-table-column>
+        </el-table>
       </el-main>
     </el-contianer>
   </el-container>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        tableData: [{
-          date: '2016-05-03',
-          name: 'Tom',
-          attendance: '1'
-        }, {
-          date: '2016-05-02',
-          name: 'John',
-          attendance: '0'
-        }, {
-          date: '2016-05-04',
-          name: 'Morgan',
-          attendance: '1'
-        }, {
-          date: '2016-05-01',
-          name: 'Jessy',
-          attendance: '1'
-        }],
-        search: ''
-      }
-    },
+import $axios from "axios";
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-03",
+          name: "Tom",
+          attendance: "1"
+        },
+        {
+          date: "2016-05-02",
+          name: "John",
+          attendance: "0"
+        },
+        {
+          date: "2016-05-04",
+          name: "Morgan",
+          attendance: "1"
+        },
+        {
+          date: "2016-05-01",
+          name: "Jessy",
+          attendance: "1"
+        }
+      ],
+      search: ""
+    };
+  },
 
-    methods: {
-      selectday(day) {
-        console.log(day)
-      }
-    },
+  methods: {
+    selectday(day) {
+      console.log(day);
+      $axios
+        .get("/rollbook/" + this.$route.params.conferenceId + "/" + day)
+        .then(res => {
+          console.log(res);
+        });
+    }
   }
+};
 </script>
 <style>
-  .is-selected {
-    color: #1989FA;
-  }
+.is-selected {
+  color: #1989fa;
+}
 
-  #calendar-day {
-    width: 100%;
-    height: 100%;
-    vertical-align: middle;
-  }
+#calendar-day {
+  width: 100%;
+  height: 100%;
+  vertical-align: middle;
+}
 </style>
