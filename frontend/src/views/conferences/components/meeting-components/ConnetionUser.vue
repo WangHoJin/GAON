d8613e61cbeaa362c0b18fcd22c735c49d041082
왕호진 <kkgmco0825@gmail.com>
2021-08-14 04:32:51 +0900

    [S05P13B101-76] 접속현황 온/오프라인 표시

--- frontend/src/views/conferences/components/meeting-components/ConnetionUser.vue
+++ frontend/src/views/conferences/components/meeting-components/ConnetionUser.vue
@@ -1,16 +1,31 @@
 <template>
-  <div>{{ clientData }},{{ idData }}</div>
+  <div>{{ clientData }},{{ idData }},{{ checkUser }}</div>
 </template>
 
 <script>
 export default {
   name: "ConnetionUser",
-
   props: {
-    subscriber: undefined
+    subscriber: undefined,
+    memberlist: []
   },
 
   computed: {
+    checkUser() {
+      const { idData } = this.getConnectionData();
+      this.memberlist.forEach(mem => {
+        console.log("id체크");
+        console.log(mem.user_id);
+        console.log(idData);
+        if (mem.user_id == idData) {
+          console.log("id가 똑같네");
+          this.color = true;
+          return this.color;
+        }
+      });
+      console.log("왜나와..");
+      return this.color;
+    },
     clientData() {
       const { clientData } = this.getConnectionData();
       return clientData;
--- frontend/src/views/conferences/components/meeting.vue
+++ frontend/src/views/conferences/components/meeting.vue
@@ -120,6 +120,11 @@
           <div v-if="connectionUser">
             <div v-if="publisher">
               <el-row>
+                <!-- <ConnetionUserList
+                  :publisher="publisher"
+                  :subscribers="subscribers"
+                  @leaveSession="leaveSession"
+                /> -->
                 <ConnetionUserList
                   :publisher="publisher"
                   :subscribers="subscribers"
@@ -195,6 +200,7 @@ import ConnetionUserList from "./meeting-components/ConnetionUserList";
 import alarm from "../../../common/mp3/alarm.mp3";
 import { h } from "vue";
 import RollBookCheck from "./meeting-components/roll-book-check.vue";
+
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 const OPENVIDU_SERVER_URL = "https://i5b101.p.ssafy.io:443";
