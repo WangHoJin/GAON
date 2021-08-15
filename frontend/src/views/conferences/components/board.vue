61d05c252d00d036af416d6dffe2169fe8cede98
김채연 <86418158+chaeyeonkim0223@users.noreply.github.com>
2021-08-15 22:44:36 +0900

    FE 아이콘 파일따로저장

--- frontend/src/views/conferences/components/board.vue
+++ frontend/src/views/conferences/components/board.vue
@@ -1,16 +1,45 @@
 <template lang="">
-  <el-container class="main-wrapper">
+  <el-container class="main-wrapper" style="background-color:green">
     <el-contianer class="main-container">
       <el-main>
-        <h1 class="mt-3">게시판의 bid : {{ $route.params.bid }}</h1>
-        <h1>여기에 게시판 글목록, 상세보기, 수정 등 구현</h1>
+        {{ boardName }}
+        {{ boardDescription }}
         <router-view></router-view>
       </el-main>
     </el-contianer>
   </el-container>
 </template>
 <script>
+import $axios from "axios";
+import { useRoute } from "vue-router";
 export default {
+  data() {
+    return {
+      boardName: "",
+      boardDescription: ""
+    };
+  },
+  async mounted() {
+    console.log("mounted");
+    const route = useRoute();
+    const url = `/boards/id/${route.params.bid}`;
+    console.log("라우터에서 bid가져오기");
+    console.log(route.params.bid);
+    await $axios
+      .get(url)
+      .then(res => {
+        console.log("get " + res);
+        console.log(res.data);
+        this.boardName = res.data.name;
+        this.boardDescription = res.data.description;
+        // this.form = res.data.post
+        // console.log("makeboard ");
+        // console.log("res.data");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 };
 </script>
 <style lang=""></style>
--- frontend/src/views/conferences/components/board/BoardList.vue
+++ frontend/src/views/conferences/components/board/BoardList.vue
@@ -5,26 +5,20 @@
         <el-table
           :data="pagedTableData"
           style="width: 100%"
-          @current-change="handleCurrentChange">
-          <el-table-column
-            prop="id"
-            label="No"
-            width="180">
+          @current-change="handleCurrentChange"
+        >
+          <el-table-column prop="id" label="No" width="180"> </el-table-column>
+          <el-table-column prop="title" label="제목" :formatter="formatter">
           </el-table-column>
-          <el-table-column
-            prop="title"
-            label="제목"
-            :formatter="formatter">
+          <el-table-column prop="nickname" label="작성자" width="180">
           </el-table-column>
-          <el-table-column
-            prop="nickname"
-            label="작성자"
-            width="180">
-          </el-table-column>
-          <el-table-column
-            prop="created_time"
-            label="작성시간"
-            width="180">
+          <el-table-column prop="created_time" label="작성시간" width="180">
+            <template #default="scope">
+              <i class="el-icon-time"></i>
+              <span style="margin-left: 10px">{{
+                scope.row.created_time
+              }}</span>
+            </template>
           </el-table-column>
         </el-table>
         <div>
@@ -33,11 +27,14 @@
             layout="prev, pager, next"
             :page-size="pageSize"
             :total="tableData.length"
-            @current-change="setPage">
+            @current-change="setPage"
+          >
           </el-pagination>
 
           <div class="right">
-            <el-button placement="right-end" @click="boardCreate()">새 글 작성</el-button>
+            <el-button placement="right-end" @click="boardCreate()"
+              >새 글 작성</el-button
+            >
           </div>
         </div>
       </el-main>
@@ -46,101 +43,105 @@
 </template>
 <script>
 import { useRouter, useRoute } from "vue-router";
-import { computed, onMounted } from 'vue';
+import { computed, onMounted } from "vue";
 import { useStore } from "vuex";
 // API
 import $axios from "axios";
-  export default {
-    mounted() {
-      // this.$nextTick(function() {
-      //       // 모든 화면이 렌더링된 후 실행합니다.
-      //   })
-      const route = useRoute()
-      const url = `/boards/${route.params.bid}/posts`;
-      console.log("mounted")
-        $axios
-          .get(url)
-          .then(res => {
-            // console.log(res.data.posts);
-            console.log(res.data.posts)
-            this.tableData = res.data.posts
-            this.tableData.reverse()
-            // console.log("makeboard ");
-            // console.log("res.data");
-          })
-          .catch(err => {
-            console.log(err);
-          });
-    },
-    updated() {
-      const route = useRoute()
-      const url = `/boards/${route.params.bid}/posts`;
-      console.log("updated")
-        $axios
-          .get(url)
-          .then(res => {
-            // console.log(res.data.posts);
-            console.log(res.data.posts)
-            this.tableData = res.data.posts
-            this.tableData.reverse()
-            // console.log("makeboard ");
-            // console.log("res.data");
-          })
-          .catch(err => {
-            console.log(err);
-          });
-    },
-    data() {
-      return {
-        tableData: [
-
-        ],
+export default {
+  mounted() {
+    // this.$nextTick(function() {
+    //       // 모든 화면이 렌더링된 후 실행합니다.
+    //   })
+    const route = useRoute();
+    const url = `/boards/${route.params.bid}/posts`;
+    console.log("mounted");
+    $axios
+      .get(url)
+      .then(res => {
+        // console.log(res.data.posts);
+        console.log(res.data.posts);
+        this.tableData = res.data.posts;
+        this.tableData.reverse();
+        // crated time 뒤에 소수점 자르기
+        this.tableData.filter(function(item, idx) {
+          item.created_time = item.created_time.substring(0, 19);
+          return item;
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  updated() {
+    const route = useRoute();
+    const url = `/boards/${route.params.bid}/posts`;
+    console.log("updated");
+    $axios
+      .get(url)
+      .then(res => {
+        // console.log(res.data.posts);
+        console.log(res.data.posts);
+        this.tableData = res.data.posts;
+        this.tableData.reverse();
+        // console.log("makeboard ");
+        // console.log("res.data");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  data() {
+    return {
+      tableData: [],
       page: 1,
       pageSize: 10,
-        formLabelWidth: '120px',
-        currentRow: null
-      }
+      formLabelWidth: "120px",
+      currentRow: null
+    };
+  },
+
+  computed: {
+    pagedTableData() {
+      return this.tableData.slice(
+        this.pageSize * this.page - this.pageSize,
+        this.pageSize * this.page
+      );
+    }
+  },
+
+  methods: {
+    formatter(row, column) {
+      return row.title;
     },
 
-    computed: {
-      pagedTableData() {
-        return this.tableData.slice(this.pageSize * this.page - this.pageSize, this.pageSize * this.page)
-      }
+    setPage(val) {
+      this.page = val;
     },
 
-    methods: {
-      formatter(row, column) {
-        return row.title;
-      },
+    boardCreate() {
+      this.$router.push({ name: "board-post-create" });
+    },
 
-      setPage (val) {
-        this.page = val
-      },
+    handleCurrentChange(val) {
+      this.currentRow = val;
+      // console.log(val.id)
+      this.$router.push({
+        name: "board-post-view",
+        params: {
+          pid: val.id
+        }
+      });
+    },
 
-      boardCreate () {
-        this.$router.push({name: 'board-post-create'})
-      },
-
-      handleCurrentChange(val) {
-        this.currentRow = val;
-        // console.log(val.id)
-        this.$router.push({
-          name: 'board-post-view',
-          params: {
-            pid: val.id
-          }
-        })
-      },
-
-      async getPosts(bid) {
+    async getPosts(bid) {
       console.log("get posts");
       const url = `/boards/${bid}/posts`;
       await $axios
         .get(url)
         .then(res => {
           // console.log(res.data.posts);
-          this.tableData = res.data.posts
-          this.tableData.reverse()
+          this.tableData = res.data.posts;
+          this.tableData.reverse();
           // console.log("makeboard ");
           // console.log("res.data");
         })
@@ -148,16 +149,15 @@ import $axios from "axios";
           console.log(err);
         });
     }
-
-    }
   }
+};
 </script>
 <style>
-  .right {
-        float: right;
-        width: 110px;
-      }
-  .gaon-button {
+.right {
+  float: right;
+  width: 110px;
+}
+.gaon-button {
   background-color: #ffd04b;
   border: none;
 }
