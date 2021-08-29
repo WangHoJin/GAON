46c7fb278502f9880470a084ff473b0236a61ec4
왕호진 <kkgmco0825@gmail.com>
2021-08-30 08:44:44 +0900

    DB 비번 설정 및 로그 삭제

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,5 +1,2 @@
-
--- create database IF NOT EXISTS ssafy_web_db collate utf8mb4_general_ci;
- create database IF NOT EXISTS gaon collate utf8mb4_general_ci;
--- DB 비밀번호 설정
-ALTER user 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'dlfgo!0!';
+-- DB 생성
+create database IF NOT EXISTS gaon collate utf8mb4_general_ci;
